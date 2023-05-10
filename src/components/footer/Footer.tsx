b0209a922c903dae13fee7ebca48053f5ffc5101
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-05-10 16:55:57 -0300

    brand and contact fully responsive

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BrandandContact from './BrandandContact';
+
+const Footer = () => {
+  return (
+    <footer className="px-6">
+      <BrandandContact />
+    </footer>
+  );
+};
+
+export default Footer;
