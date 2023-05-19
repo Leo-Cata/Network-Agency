604d665712215421f8ead6e583b87938460bf8b2
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-05-19 18:19:08 -0300

    footer done

--- src/components/footer/Links.tsx
+++ src/components/footer/Links.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LinksServices from './LinksServices';
 import LinksDevelopment from './LinksDevelopment';
 import LinksDesign from './LinksDesign';
@@ -6,17 +6,30 @@ import LinksBunch from './LinksBunch';
 import Contact from './Contact';
 
 const Links = () => {
+  const [viewportWidth, setViewportWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="mt-7 flex flex-wrap justify-center gap-[27px] font-openSans text-white xl:justify-start xl:gap-0 xl:space-x-[60px]">
+    <div className="mt-7 flex flex-wrap justify-start gap-[27px] font-openSans text-white xl:justify-center xl:gap-0 xl:space-x-[60px]">
       <Contact />
 
-      <div className="xl:order-2 xl:flex xl:space-x-[60px]">
+      <div className=" xl:order-2 xl:flex xl:space-x-[60px]">
         <LinksServices />
-        <LinksDevelopment />
+        {viewportWidth > 1279 ? <LinksDesign /> : <LinksDevelopment />}
       </div>
 
       <div className="xl:order-3 xl:flex xl:space-x-[60px]">
-        <LinksDesign />
+        {viewportWidth > 1279 ? <LinksDevelopment /> : <LinksDesign />}
+
         <LinksBunch />
       </div>
     </div>
