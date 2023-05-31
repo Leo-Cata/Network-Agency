c22761e082dc5c63c0527d51712646581441b05c
Leo-Cata <63213371+Leo-Cata@users.noreply.github.com>
2023-05-31 13:03:06 -0300

    footer done, fixing paddings and margins

---
+++ src/container/Wrapper.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+interface Props {
+  children: React.ReactNode;
+}
+
+const Wrapper = ({ children }: Props) => {
+  return <div className="mx-6 md:mx-16 2xl:mx-[300px]">{children}</div>;
+};
+
+export default Wrapper;
